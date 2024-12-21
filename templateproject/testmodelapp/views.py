1343f49cb0cbb47d97e1f95f0b814eef94df480c
nirmaldash1988 <nirmal.dash1988@gmail.com>
2024-12-21 17:52:29 +0530

    21-12-2024-class

--- templateproject/testmodelapp/views.py
+++ templateproject/testmodelapp/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 from testmodelapp import models
+import json
 # Create your views here.
+
+
+def student_detail_view(request, id):
+    obj = models.StudentModel.objects.get(id=id)
+    return render(request, 'testmodelapp/student_info.html', {"item": obj})
+
+
+def student_update_view(request, id):
+    obj = models.StudentModel.objects.get(id=id)
+    if request.method == "POST":
+        mark = int(request.POST.get("mark"))
+        obj.mark = mark + 20
+        obj.save()
+        return JsonResponse({"message": "Mark has been updated!!"})
+    return render(request, 'testmodelapp/update.html', {"item": obj})
 
 
 def student_view(request):
     obj = models.StudentModel.objects.all()
+    # obj = models.StudentModel.objects.filter(mark__gt=400)
     # new_item = []
     # for item in obj:
     #     result = {
@@ -16,4 +34,4 @@ def student_view(request):
     #     }
     #     new_item.append(result)
     # html = f"<h2>The result is {new_item}</h2>"
-    return render(request, 'testmodelapp/student_info.html', {"result": obj})
+    return render(request, 'testmodelapp/home.html', {"result": obj})
