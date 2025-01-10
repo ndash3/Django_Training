a62baed7479cf591a939831363c9c71dd7cc82cc
nirmaldash1988 <nirmal.dash1988@gmail.com>
2025-01-10 20:50:28 +0530

    admin-panel-10-01-2024

--- templateproject/testmodelapp/admin.py
+++ templateproject/testmodelapp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from testmodelapp.models import StudentModel
 
-admin.site.register(StudentModel)
+# admin.site.register(StudentModel)
 # Register your models here.
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'roll', 'mark', 'name_length', 'is_student_pass')
+    search_fields = ('name',)
+    list_filter = ('name', 'age')
+    ordering = ('-mark',)
+    list_editable = ('mark',)
+
+
+    def name_length(self, obj):
+        return len((obj.name))
+
+    def is_student_pass(self, obj):
+        return obj.mark > 300
+
+
+admin.site.register(StudentModel, StudentAdmin)
